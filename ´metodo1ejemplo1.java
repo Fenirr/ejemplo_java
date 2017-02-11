c4dd0c943ce32d3354247560df113460b92a2b1e
Fenirr <162t0305@itsm.edu.mx>
2017-02-11 15:47:52 -0600

    metodojava
    ejemplo de metodo uso java

---
+++ ´metodo1ejemplo1.java
@@ -0,0 +1,15 @@
+package metodos1;
+
+public class ejemplo1 {
+public static void main (String[]args){
+	multiplicar();
+}
+	public static void multiplicar(){
+		int d=60;
+		int f=40;
+		int multiplicar= d*f;
+		System.out.println("la respuesta es :"+ multiplicar);
+		
+	}
+}
+
